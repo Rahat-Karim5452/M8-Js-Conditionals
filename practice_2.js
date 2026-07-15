099dff10705d6ef4ffdc298505d57e1c49368e16
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-15 23:05:06 +0600

    Practice task 2 Done

---
+++ practice_2.js
@@ -0,0 +1,7 @@
+//todo:Guess the result of "5" == 5 and "5" === 5 first, then write code to verify your guess.
+
+//*My Guess: "5"==5(true) "5"===5 (false)
+const a = 5;
+const b = "5";
+console.log(a == b);
+console.log(a === b);
