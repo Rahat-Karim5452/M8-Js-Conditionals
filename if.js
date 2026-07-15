b66d41c789a0a5bedfc77457cf14d150c76f1cfb
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-15 19:19:30 +0600

    if condition practice

---
+++ if.js
@@ -0,0 +1,34 @@
+/* if (true)
+{
+    console.log('Inside the condition');
+} */
+/* if(false)
+{
+    console.log('Inside the condition');
+} */
+console.log("---------------------");
+if (8 > 3) {
+  console.log("Inside the Condition");
+  console.log("8 is bigger than 3");
+}
+if (50 <= 50) {
+  console.log("Comming inside the condition");
+}
+
+console.log("---------------------");
+const price = 1400;
+
+if (price < 500) {
+  console.log("price is lower");
+}
+const money = 2000;
+if (price < money) {
+  console.log("i wil buy it");
+}
+console.log("---------------------");
+
+const name = 'Potato';
+if (name === 'Potato')
+{
+    console.log('we are mr.potato');
+}
