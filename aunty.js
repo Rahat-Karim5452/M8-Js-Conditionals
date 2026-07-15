f28b634860f3b159cce4c0da1a40276ab39391c1
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-15 18:50:26 +0600

    uses of Arithmetic operator

---
+++ aunty.js
@@ -0,0 +1,18 @@
+
+/* Using : 
+1.>,<
+2.==,===
+3.>=,<=
+4.!=,!==
+5.&&,|| 
+*/
+const a  = 10;
+const b = 7;
+
+console.log(a>b); //output: true 
+console.log(a<b);//output: false
+console.log(a===b);//output: false
+console.log(a==b);//output: false 
+console.log(a>=b);//output:true
+console.log(a<=b);//output: false 
+console.log(a!==b);//output:true 
