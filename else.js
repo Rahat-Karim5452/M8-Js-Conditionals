68c3443a9ba9bb2ceeec5d143a16cd56f069699e
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-15 22:08:53 +0600

    multiple js added

--- else.js
+++ else.js
@@ -1,23 +1,25 @@
+//?Extention: Better Comment
+//todo: Better Comment
+//!Better Comment
+//*Highlight Comment
+
 const rickshawFare = 200;
 
 if (rickshawFare < 50) {
-    console.log('mama hood tule den');
-    console.log('go slowly');
-}
-else {
-    console.log('walking is also exercising');
-    console.log('I am a situational runner');
+  console.log("mama hood tule den");
+  console.log("go slowly");
+} else {
+  console.log("walking is also exercising");
+  console.log("I am a situational runner");
 }
 
-console.log('=======================')
+console.log("=======================");
 const x = 200;
 const y = 20;
 
-if(x!== y){
-    const z = x + y;
-    console.log('value of z: ',z);
+if (x !== y) {
+  const z = x + y;
+  console.log("value of z: ", z);
+} else {
+  console.log("they are equal equal mukul mukul");
 }
-else{
-    console.log('they are equal equal mukul mukul')
-}
-
