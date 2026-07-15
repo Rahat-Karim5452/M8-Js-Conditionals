621608655401d16899450b320dd4bef1ba2a98ae
Rahat Karim <rahatkarim5452@gmail.com>
2026-07-15 23:11:14 +0600

    Practice task 3 Done

---
+++ practice_3.js
@@ -0,0 +1,9 @@
+//todo:Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+const isRaining = true;
+if (isRaining) {
+  console.log("Take an Umbrella");
+}
+
+if (!isRaining) {
+  console.log(".........");
+}
